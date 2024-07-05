70d38b3fddc106c3488358630f15a3736987e4ac
eggsandwich-dev <cpitre1@uncc.edu>
2024-07-05 17:15:50 -0400

    updated navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,14 +11,11 @@ interface NavbarItemProps {
 
 const NavbarItem: React.FC<NavbarItemProps> = ({itemLink, content, isActive}) => {
     return (
-        <li className='py-2 px-4'>
-            <Link
-                to={itemLink}
-                className={`text-white mr-4 p-2 rounded text-center transform transition-transform duration-500 ${isActive ? 'bg-rose-900' : 'hover:bg-rose-700'} hover:scale-110`}
-            >
+        <Link to={itemLink}>
+            <li className={`mt-4 py-2 px-4 text-white mr-4 p-2 rounded bg-opacity-50 text-center transform transition-transform duration-500 ${isActive ? 'bg-rose-900 hover:scale-110' : 'hover:bg-rose-700 hover:scale-110'}`}>
                 {content}
-            </Link>
-        </li>
+            </li>
+        </Link>
     )
 }
 
@@ -26,20 +23,16 @@ const Navbar: React.FC = () => {
     const location = useLocation();
 
     return (
-        <div className='h-screen w-64 bg-rose-800 text-white'>
-            <div className='p-4'>
-                <div className="flex items-center">
-                    <Link to="/">
-                        <img src={logo} alt='Website Logo' className="h-12 w-auto object-contain rounded transform transition-transform duration-500 hover:scale-110" loading="lazy" />
-                    </Link>
-                    <span className="ml-3 text-white text-xl font-bold">Chris Pitre</span>
-                </div>
-                <ul className="mt-4">
-                    <NavbarItem itemLink="/" content="Home" isActive={location.pathname === '/'} />
-                    <NavbarItem itemLink="/about" content="About Me" isActive={location.pathname === '/about/'} />
-                    <NavbarItem itemLink="/projects" content="Projects" isActive={location.pathname === '/projects/'} />
-                </ul>
-            </div>
+        <div className='h-screen w-64 p-4 text-white'>
+            <Link to="/" className='justify-between flex py-2 items-center transform transition-transform duration-500 hover:scale-110'>
+                <img src={logo} alt='Website Logo' className="h-12 w-auto object-contain rounded" loading="lazy" />
+                <span className=" text-white text-xl font-bold">Chris Pitre</span>
+            </Link>
+            <ul className="mt-4">
+                <NavbarItem itemLink="/" content="Home" isActive={location.pathname === '/'} />
+                <NavbarItem itemLink="/about" content="About Me" isActive={location.pathname === '/about/'} />
+                <NavbarItem itemLink="/projects" content="Projects" isActive={location.pathname === '/projects/'} />
+            </ul>
         </div>
     )
 }
