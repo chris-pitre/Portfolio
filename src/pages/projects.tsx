e015d06659e5e4c9ba58f2f34a825a820f4e558a
eggsandwich-dev <cpitre1@uncc.edu>
2024-07-06 18:29:36 -0400

    better project justification

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -7,7 +7,7 @@ const ProjectPage = () => {
         <main>
             <Layout pageTitle="Projects">
                 <p className='text-4xl mt-16 p-4'>Here are some of my projects:</p>
-                <div className='flex justify-between flex-row gap-4 p-6 flex-wrap max-w-7xl items-start'>
+                <div className='flex flex-row gap-4 p-6 flex-wrap max-w-7xl items-start justify-center'>
                     <Card image={require('../img/test.jpg').default}/>
                     <Card />
                     <Card image={require('../img/test2.jpg').default}/>
