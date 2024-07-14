7afdbb81a046eb81cde208115da436fbb017fddb
eggsandwich-dev <cpitre1@uncc.edu>
2024-07-14 00:41:53 -0400

    started working on projects page, added projects

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,24 +1,53 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 interface CardProps {
     image?: string;
+    title: string;
+    description: string;
+    link: string;
+    date: string;
+    github_link?: string;
+    external_link?: string;
+    external_desc?: string;
 }
 
-const Card: React.FC<CardProps> = ({image}) => {
-    let image_tag;
+const Card: React.FC<CardProps> = ({image, title, description, link, date, github_link, external_link, external_desc}) => {
+    let image_tag, github_button, external_button;
 
     if(image){
-        image_tag = <img src={image} alt="test img" className={`rounded-t w-full h-auto object-contain`}/>
+        image_tag = <img src={image} alt="test img" className={`rounded-t w-full h-auto object-cover`}/>
     } else {
         image_tag = <div className='hidden'></div>
     }
 
+    if(github_link){
+        github_button = <Link to={github_link} className={`bg-rose-900 mx-4 py-2 px-4 text-white rounded bg-opacity-50 text-center hover:bg-rose-700`}>Github</Link>
+    } else {
+        github_button = <div className='hidden'></div>
+    }
+
+    if(external_link){
+        if (!external_desc){
+            external_desc = "Live App"
+        }
+        external_button = <Link to={external_link} className={`bg-rose-900 mx-4 py-2 px-4 text-white rounded bg-opacity-50 text-center hover:bg-rose-700`}>{external_desc}</Link>
+    } else {
+        external_button = <div className='hidden'></div>
+    }
+
     return (
-        <div className='max-w-sm h-auto bg-slate-400 bg-opacity-30 rounded shadow-md'>
+        <div className='group max-w-sm h-auto bg-slate-400 bg-opacity-30 overflow-hidden rounded shadow-md transition-all hover:scale-105'>
             {image_tag}
-            <div className='p-6'>
-                <h1 className='text-2xl font-bold'>Test Project</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quod ipsa earum. Maiores, dolores? Nostrum, tempora distinctio similique aliquam quae ut, ipsum commodi laudantium unde veritatis deserunt possimus incidunt! Totam?</p>
+            <div className='p-6 block'>
+                <h1 className='text-2xl font-bold'>{title}</h1>
+                <p className='text-sm'>{date}</p>
+                <p className='text-md'>{description}</p>
+                <ul className="list-none mt-4 flex flex-col space-y-2">
+                    <Link to={link} className={`bg-rose-900 mx-4 py-2 px-4 text-white rounded bg-opacity-50 text-center hover:bg-rose-700`}>Read More</Link>
+                    {github_button}
+                    {external_button}
+                </ul>
             </div>
         </div>
     );
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -8,14 +8,60 @@ const ProjectPage = () => {
             <Layout pageTitle="Projects">
                 <p className='text-4xl mt-16 p-4'>Here are some of my projects:</p>
                 <div className='flex flex-row gap-4 p-6 flex-wrap max-w-7xl items-start justify-center'>
-                    <Card image={require('../img/test.jpg').default}/>
-                    <Card />
-                    <Card image={require('../img/test2.jpg').default}/>
-                    <Card image={require('../img/test.jpg').default}/>
-                    <Card image={require('../img/test2.jpg').default}/>
-                    <Card />
-                    <Card image={require('../img/test.jpg').default}/>
-                    <Card />
+                    <Card
+                        link="/"
+                        title="Final Nail"
+                        description="Game made in 1 week for Dungeon Crawler Game Jam 2024. Made with Ryan Pham and Thomas Benfield III. Scored #78/#167."
+                        image={require("../img/final_nail.png").default}
+                        date="March 2024"
+                        github_link='https://github.com/chris-pitre/FinalNail'
+                        external_link='https://vextorm.itch.io/final-nail'
+                        external_desc='Play'
+                    />
+                    <Card
+                        link="/"
+                        title="Mosqit 97"
+                        description="Game made in 48 Hours for UNC Charlotte 2024 Spring Game Jam. Scored #6/#13."
+                        image={require("../img/mosqit.png").default}
+                        date="March 2024"
+                        github_link='https://github.com/chris-pitre/Mosqit-97'
+                        external_link='https://theeggsandwich.itch.io/mosqit-97'
+                        external_desc='Play'
+                    />
+                    <Card
+                        link="/"
+                        title="Classifying Veg vs Non-Veg food at Social 704"
+                        description="Computer Vision Project to classify vegetarian and non-vegetarian foods at UNC Charlotte's Social 704 dining hall. Made with a group."
+                        image={require("../img/social704.jpg").default}
+                        date="November-December 2023"
+                        github_link='https://github.com/chris-pitre/Veg-vs-Non-Veg-Identification-at-Social-704'
+                    />
+                    <Card
+                        link="/"
+                        title="Soundscape"
+                        description="Game made for ITCS-4236/5236 Artificial Intelligence for Computer Game at UNC Charlotte. Made with a group."
+                        image={require("../img/soundscape.gif").default}
+                        date="August-December 2023"
+                        github_link='https://github.com/chris-pitre/Soundscape'
+                    />
+                    <Card
+                        link="/"
+                        title="Gravel"
+                        description="Game made for ITCS-4231/5231 Advanced Game Design and Development at UNC Charlotte. First 3D Unity Game."
+                        image={require("../img/gravel.png").default}
+                        date="August 2023"
+                        github_link='https://github.com/chris-pitre/Gravel'
+                    />
+                    <Card
+                        link="/"
+                        title="Dealer's Choice"
+                        description="Game made in 48 hours for GMTK Game Jam 2023. Made with Ryan Pham. Scored #414/#6752."
+                        image={require("../img/dealers_choice.gif").default}
+                        date="July 2023"
+                        github_link='https://github.com/chris-pitre/Dealers-Choice'
+                        external_link='https://vextorm.itch.io/dealers-choice'
+                        external_desc='Play'
+                    />
                 </div>
             </Layout>
         </main>
